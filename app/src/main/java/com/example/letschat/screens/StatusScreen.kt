7a81eee6cff66864d679d3ad637cc25f30a8deed
Mayank Choudhary <mayankchoudhary9941@gmail.com>
2024-07-01 06:49:19 +0530

    status screen is ready

--- app/src/main/java/com/example/letschat/screens/StatusScreen.kt
+++ app/src/main/java/com/example/letschat/screens/StatusScreen.kt
@@ -1,7 +1,131 @@
 package com.example.letschat.screens
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Edit
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.letschat.CommonDivider
+import com.example.letschat.CommonProgressBar
+import com.example.letschat.CommonRow
+import com.example.letschat.DestinationScreen
+import com.example.letschat.MMViewModel
+import com.example.letschat.TitleText
+import com.example.letschat.navigateTo
 
 @Composable
-fun StatusScreen() {
+fun StatusScreen(navController: NavController, viewModel: MMViewModel) {
+
+    val inProcess = viewModel.inProgressStatus.value
+
+    if (inProcess) {
+        CommonProgressBar()
+    } else {
+        val statuses = viewModel.status.value
+        val userData = viewModel.userData.value
+        val myStatus = statuses.filter {
+            it.user.userId == userData?.userId
+        }
+        val otherStatus = statuses.filter {
+            it.user.userId != userData?.userId
+        }
+        val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) {
+                uri->
+            uri?.let {
+                viewModel.uploadStatus(uri)
+            }
+        }
+        Scaffold(
+            floatingActionButton = {
+                FAB {
+                    launcher.launch("image/*")
+                }
+            },
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it)
+
+                ) {
+                    TitleText(text = "Status")
+                    if (statuses.isEmpty()) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(text = "No Status")
+                        }
+
+                    } else {
+                        if (myStatus.isNotEmpty()) {
+                            CommonRow(
+                                imageUrl = myStatus[0].user.imageUrl, name = myStatus[0].user.name
+                            ) {
+                                navigateTo(
+                                    navController,
+                                    route = DestinationScreen.SingleStatus.createRoute(myStatus[0].user.userId!!)
+                                )
+                            }
+                            CommonDivider()
+                            val uniqueUsers = otherStatus.map { it.user }.toSet().toList()
+                            LazyColumn(
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                items(uniqueUsers) { user ->
+                                    CommonRow(
+                                        imageUrl = user.imageUrl, name = user.name
+                                    ) {
+                                        navigateTo(
+                                            navController,
+                                            DestinationScreen.SingleStatus.createRoute(user.userId!!)
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    BottomNavigationMenu(BottomNavigationItem.STATUSLIST, navController)
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun FAB(
+    onFabClick: () -> Unit
+) {
+    FloatingActionButton(
+        onClick = onFabClick,
+        containerColor = Color.Green,
+        shape = CircleShape,
+        modifier = Modifier.padding(40.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.Edit,
+            contentDescription = "Add Status",
+            tint = Color.Black
+        )
+    }
 }
